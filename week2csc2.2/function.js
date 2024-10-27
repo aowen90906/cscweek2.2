4f58e56a3cef927e164c75ba1a047bc7d5e2b2f1
aowen90906 <AOWEN90906@uat.edu>
2024-10-27 17:00:40 -0400

    Add files via upload

--- week2csc2.2/function.js
+++ week2csc2.2/function.js
@@ -1,3 +1,6 @@
+//Anthony Owen, Assignment 2.2: The Building Blocks for Javascript - Functions, added game of heads or tails (winner buys parts when heads is triggered, passes when tails is triggered)//
+// uses random math generator, uses harleyhead.jpg image when heads is triggered, uses tailsection.jpg when tails is triggered//
+
 function flipcoin()
 {
     var player1 = Math.ceil(Math.random()*2);
@@ -6,11 +9,13 @@ function flipcoin()
 
     if (player1 == 1)
     {
+        //inputs player1 headimage when heads is triggered//
         document.getElementById("player1Img").src = headimage;
     }
     
     if (player1 == 2)
     {
+        //inputs player1 tailimage when tails is triggered//
         document.getElementById("player1Img").src = tailimage;    
     }
     
