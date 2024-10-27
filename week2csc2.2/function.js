863a84ae2076b091c5339c8ca1fcca9003964fb1
aowen90906 <AOWEN90906@uat.edu>
2024-10-27 13:41:07 -0400

    Add files via upload

--- week2csc2.2/function.js
+++ week2csc2.2/function.js
@@ -2,6 +2,15 @@ function flip-a-coin()
 {
     var player1 = Math.ceil(Math.random()*2);
     var headimage = "harleyhead.jpg";
-    var tailimage = "tailsection.jpg":
+    var tailimage = "tailsection.jpg";
+
+    if (player1 == 1)
+    {
+        document.getElementById("player1Img").src = headimage;
+    }
+    else (player1 == 2)
+    {
+        document.getElementById("player1Img").src = tailimage:
+    }
 }
 
